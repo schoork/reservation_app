bbcca609c850c3716f1f149bb23e70a4be993d50
Sam Williams <sblwilliams@gmail.com>
2019-05-08 05:18:44 -0500

    import and create client

--- App.js
+++ App.js
@@ -8,12 +8,10 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import ApolloClient from 'apollo-boost';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const client = new ApolloClient({
+  uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev'
 });
 
 type Props = {};
