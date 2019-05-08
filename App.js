c4c16c934ffb2b8884cafe4c7bb3540133d79777
Sam Williams <sblwilliams@gmail.com>
2019-05-08 06:04:35 -0500

    log reservations to console

--- App.js
+++ App.js
@@ -9,19 +9,33 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 
 const client = new ApolloClient({
   uri: 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev'
 });
 
+initialQuery = client.query({
+  query: gql`
+    query reservations {
+      reservations {
+        name
+        hotelName
+      }
+    }
+  `,
+})
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
+
 type Props = {};
 export default class App extends Component<Props> {
+  initialQuery;
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
